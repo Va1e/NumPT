23f11e2c9d4ffeae04583456742420bb1c7abc69
vale <vale@git.olpp.net>
2013-12-07 18:28:06 +0100

    Aufgaben von Blatt 1 hinzgefügt

---
+++ 1.1.m
@@ -0,0 +1,40 @@
+%
+% while true; do curl olpp.net/~marco/aufgabe11.m > aufgabe11.m; sleep 1; done
+%
+
+%Blatt 1, Aufgabe 1
+format long
+%n=[3:3:30];
+x=-5.5;
+naeherung1 = naeherung2 = 0;
+
+wert_vom_blatt = 0.0040867714
+wert_aus_octave=exp(-5.5)
+
+puts("a)\n");
+
+for n=3:3:30
+        e=1;
+            for k=1:n
+                        s=(x^k)/(factorial(k));
+                                e=e+s;
+                                    end
+                                        naeherung1 = e
+                                    end
+
+                                    absolut_1 = (naeherung1 - wert_aus_octave)
+                                    relativ_1 = absolut_1/wert_vom_blatt
+
+                                    puts("b)\n");
+                                    for n=3:3:30
+                                            e=1;
+                                                for k=1:n
+                                                            s=((-x)^k)/factorial(k);
+                                                                    e=e+s;
+                                                                        end
+                                                                            naeherung2 = 1/e
+                                                                        end
+                                                                        puts("\n");
+
+                                                                        absolut_2 = (naeherung2 - wert_aus_octave)
+                                                                        relativ_2 = absolut_1/wert_vom_blatt
---
+++ 1.2.m
@@ -0,0 +1,34 @@
+%Aufgabe 1.2
+
+P7a = @(x) x.^7 - 7.*x.^6 + 21.*x.^5 - 35 .* x.^4 + 35 .* x.^3 - 21 .* x.^2 +  7 .* x .- 1
+P7b = @(x) (x-1).^7
+P7c = @(x) ((((((x - 7).*x + 21).*x - 35).*x + 35).*x - 21).*x+7).*x - 1
+
+x=single([0.8:5e-5:1.2]);
+genau=P7b([0.8:5e-5:1.2]);
+
+subplot(2,2,1);
+y=P7a(x);
+plot(x, y); %Wie zu erwarten viel Ausloeschung; immer +- abwechselnd
+delta1 = sqrt(sum((genau-y).^2));
+title('monomsarstellung');
+
+subplot(2,2,2);
+y=P7b(x);
+plot(x, y); % Diese hier ist monoton, da zuerst (x-1) ausgerechnet wird, was monton ist, und dann ^7, was ebenfalls monoton ist.
+delta2 = sqrt(sum((genau-y).^2));
+title('(x-1)^7');
+
+subplot(2,2,3); % Auch nicht schön: immernoch 6 +-.
+y=P7c(x);
+plot(x, P7c(x));
+delta3 = sqrt(sum((genau-y).^2));
+title('horner');
+
+subplot(2,2,4);
+bar([delta1, delta2, delta3]);
+title('Abweichung');
+
+puts('--- Return to close. ---\n');
+P8(4)
+pause();
