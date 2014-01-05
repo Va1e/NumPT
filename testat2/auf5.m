cb604bfbb6bfe78be194d97ae41ceedf0b658826
Marco Zech <marco@debian>
2014-01-05 16:22:35 +0100

    blatt6 hinzugefügt

--- testat2/auf5.m
+++ testat2/auf5.m
@@ -6,9 +6,9 @@ function [c] = DFT(f)
 	x=linspace(0,2*pi,length(f))
 	for k=1:length(f)
 	  be=m*f(k);
-	  for l=0:1:n
+	  for l=0:1:n;
 	    zu=m*f(l+1)*exp(-i*l*x(k));
-	    be=be+zu;
+	    be=be+zu
 	  end
 	  c(k)=be;
 	end
