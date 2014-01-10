3b81155564cbfcd8a7d93d632a0bc15a553b13fc
Valentin Olpp <ox98aget@cip028.physik.uni-erlangen.de>
2014-01-10 09:58:30 +0100

    zwei bilder dazugefügt

--- testat2/5.1ab.m
+++ testat2/5.1ab.m
@@ -5,9 +5,14 @@ function [R] = myImgCompression(img, compr, imgout)
 	end
 	compress=compr/100
 	B=imread(img);
-	s=size(B)
-	%Es gibt heute keie S/W Bilder mehr => nur 3D Matrizen
-	for c=1:1:s(3)
+	s=size(B);
+	%Fall von 2D Matrizen/SW-Bildern wird abgedeckt
+	if size(s)=2
+		colors=1
+	else
+		colors=s(3)
+	end
+	for c=1:1:colors
 		for y=1:1:s(1)
 			transformed=fft(double(B(y,:,c)));
 			[sor,ind]=sort(abs(transformed));
@@ -19,8 +24,7 @@ function [R] = myImgCompression(img, compr, imgout)
 		end
 	end
 	imwrite(B,imgout)
-	%image(B)
 end
-for index=0:10:100
-	myImgCompression("largecat.bmp",index,[sprintf("catcomp%.3d.jpg", index)])
+for index=10:10:90
+	myImgCompression("zebra.bmp",index,[sprintf("zebracomp%.3d.jpg", index)])
 end
